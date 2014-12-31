94e2b31a1e7d42373d372320d1bb2466d3eae485
syg <sgogel@ecedi.fr>
2014-12-31 10:29:44 +0100

    bump version

--- less-compile.php
+++ less-compile.php
@@ -3,7 +3,7 @@
 Plugin Name: Less Compile
 Plugin URI: http://www.ecedi.fr/
 Description: Compile all Less files in Theme Stylesheet Directory
-Version: 2.0
+Version: 2.1
 Author: Julien Zerbib, Sylvain Gogel <sgogel@ecedi.fr>
 Author URI: http://www.ecedi.fr/
 
